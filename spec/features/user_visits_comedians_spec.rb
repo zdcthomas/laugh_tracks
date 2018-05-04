d2cd0f5daebe818b182fe7401cb167dae5315e29
zdcthomas <zdcthomas@yahoo.com>
2018-05-04 10:14:28 -0600

    Introduces user stories in rspec file

---
+++ spec/features/user_visits_comedians_spec.rb
@@ -0,0 +1,16 @@
+RSpec.describe 'A User' do
+  describe 'visits /comedians' do
+    it 'Displays a list of Comedians with name and age' do
+    end
+    it 'Displays a list of the comedians specials' do
+    end
+    it 'Displays the average age for all comedians' do
+    end
+    it 'Displays a count of the specials for each comedian'do 
+    end
+  end
+  describe 'queries for all comedians of a certain age' do
+    it 'Displays a list comedians of a ceratin age' do
+    end
+  end
+end
