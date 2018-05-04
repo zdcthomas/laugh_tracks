09dcadde3044acf9963d5d2a3028da50a75c7060
zdcthomas <zdcthomas@yahoo.com>
2018-05-04 10:22:33 -0600

    TESTS displays list of Comedians

--- spec/features/user_visits_comedians_spec.rb
+++ spec/features/user_visits_comedians_spec.rb
@@ -1,6 +1,15 @@
 RSpec.describe 'A User' do
   describe 'visits /comedians' do
     it 'Displays a list of Comedians with name and age' do
+      iliza = Comedian.create(name: "Iliza Shlesinger", age: 34)
+      hannibal = Comedian.create(name: "Hannibal Buress", age: 34)
+      mike = Comedian.create(name: "Mike Birbiglia", age: 39)
+      visit '/comedians'
+      expect(page).to have_content('Mike Birbiglia')
+      expect(page).to have_content('Hannibal Buress')
+      expect(page).to have_content('Iliza Shlesinger')
+      expect(page).to have_content('age: 34')
+      expect(page).to have_content('age: 39')
     end
     it 'Displays a list of the comedians specials' do
     end
